c819be0f4f86888c048c36332f42e90435b52a8a
ShaitoMo <shaitomohammad695@gmail.com>
2026-08-03 20:21:55 +0300

    patch user info by idupdated

--- src/types/user.ts
+++ src/types/user.ts
@@ -12,3 +12,14 @@ export type User = z.infer<typeof UserValidateSchema>
 export type CreateUserInput = Omit<User, 'password'> & { passwordHash: string }
 
 export type SafeUser = Omit<CreateUserInput, 'passwordHash'> & { userId: number, isActive: boolean, createdAt: Date }
+
+export const UserUpdateSchema = z.object({
+    name: z.string().min(3).max(50).optional(),
+    phone: z.string().min(1).optional(),
+    roleId: z.number().int().positive().optional(),
+    isActive: z.boolean().optional(),
+}).refine(data => Object.values(data).some(value => value !== undefined), {
+    message: 'At least one field must be provided',
+})
+
+export type UpdateUserInput = z.infer<typeof UserUpdateSchema>
